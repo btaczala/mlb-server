266cb75f6c3b5cc3f868c592876b45037ec8c763
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-03-29 13:25:58 +0200

    travis-ci: Add BUILD_ALL flag for CI

--- travis/build.sh
+++ travis/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cmake_command="cmake /home/sources/ -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DDISABLE_3rdParty=ON -GNinja -DSANITIZER=$SANITIZER"
+cmake_command="cmake /home/sources/ -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DBUILD_ALL=$BUILD_ALL -GNinja -DSANITIZER=$SANITIZER"
 
+echo $cmake_command
 docker run --rm -e CC -e CXX \
     -v `pwd`:/home/sources bartekt/mlb-docker /bin/bash \
     -c "cd /home/ && rm -rfv build && mkdir build && cd build && $cmake_command && ninja && MLB_SERVER_LOG_LEVEL=debug ctest --repeat-until-fail 10 -V; coredumpctl info"
