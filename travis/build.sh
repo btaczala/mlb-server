a0349214efe7dab10664e5b7835c08c9c20a111c
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-03-29 11:05:29 +0200

    travis-ci: Proper CC and CXX export

--- travis/build.sh
+++ travis/build.sh
@@ -2,6 +2,6 @@
 
 cmake_command="cmake /home/sources/ -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DDISABLE_3rdParty=ON -GNinja -DSANITIZER=$SANITIZER"
 
-docker run --rm \
+docker run --rm -e CC -e CXX \
     -v `pwd`:/home/sources bartekt/mlb-docker /bin/bash \
     -c "cd /home/ && rm -rfv build && mkdir build && cd build && $cmake_command && ninja && ctest --repeat-until-fail 10"
