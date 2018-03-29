69e64459aaaeed7bb5a1ab1cafe3ff56c76fa52f
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-03-29 10:49:43 +0200

    integrate cxxopts

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include "database.hpp"
+#include "databaseimpl.hpp"
+#include "log.h"
 #include "server.hpp"
 
-#include "databaseimpl.hpp"
+#include <cxxopts.hpp>
 
-extern void setupLogger(bool);
+int main(int argc, const char **argv) {
 
-int main(int argc, char *argv[]) {
-    setupLogger(true);
+    cxxopts::Options opts{"mlb-server", "REST server for MLB"};
+
+    opts.add_options()("v,verbose", "Enable verbose mode")("h,help",
+                                                           "Show help");
+
+    const auto result = opts.parse(argc, argv);
+
+    if (result.count("h")) {
+        std::cout << opts.help({""}) << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    setupLogger(result.count("d") != 0);
+
     mlb::data::DatabaseImpl db;
 
     mlb::server::Server s;
